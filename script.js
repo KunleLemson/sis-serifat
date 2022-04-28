3f093e04eeac48a952d4161b06a2cfb1f2e76bd5
AYKdgreat <dalemoayokunlephilip@gmail.com>
2022-04-28 16:30:29 +0100

    2022: Added slideshow (w3schools), caption to gallery

---
+++ script.js
@@ -0,0 +1,23 @@
+// let display = document.querySelector("#display");
+// console.log(display.src);
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+//   console.log(slides);
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+} 
